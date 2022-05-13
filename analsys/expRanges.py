ac95aa38d84ffaf6b745bc71fa02fe49d730f77b
GalileoCap <galileocapp@gmail.com>
2022-05-13 02:47:54 -0300

    OK: Skipping ahead when continuing

--- analsys/expRanges.py
+++ analsys/expRanges.py
@@ -4,74 +4,45 @@ from plot import *
 from utils import *
 
 class ExpRanges:
-	def __init__(self, name, ranges, mode = 'replace'): #U: Runs the program with different values for k, a, 
-		print(f'ExpRanges START name {name} ranges {ranges}, mode {mode}')
+	def __init__(self, name, ranges): #U: Runs the program with different values for k, a, 
+		print(f'ExpRanges START name {name} ranges {ranges}')
 		self.name = name
 		self.path = exp_path(name, 'expRanges')
-		self.df_fpath = df_fpath(self.path, 'df') #TODO: Rename
-		self.tmpdf_fpath = df_fpath(self.path, '.tmpdf') #TODO: Rename
+		self.df_fpath = df_fpath(self.path, 'df') 
 		self.ranges = ranges
-		self.mode = mode
 
-		if self.mode == 'read': self.read()
-		else: self.run()
+		self.run()
 		self.plot()
 
 	def run(self):
-		print(f'ExpRanges RUN')
+		lastRange = 0
+		try: df = pd.read_csv(self.df_fpath) #A: Read previous df and skip ahead
+		except FileNotFoundError:	print(f'ExpRanges RUN df not found, starting from scratch')
+		else:
+			lastRange = len(df) / 2 #NOTE: /2 because we're adding two rows per iteration
+			print(f'ExpRange RUN continuing from {lastRange}')
 
-		startRanges = {'train': 0, 'k': 0, 'n': 0}
-		if self.mode == 'continue': #A: Read tmpdf and skip ahead
-			try:
-				tmpdf = pd.read_csv(self.tmpdf_fpath)
-			except FileNotFoundError:
-				print(f'ExpRanges RUN tmpdf not found, starting from scratch')
-			else:
-				startRanges.update({
-					'train': len(tmpdf['trainSz'].unique()),
-					# 'k': len(tmpdf['k'].unique()),
-					# 'n': len(tmpdf['n'].unique())
-				})
-				#TODO: More depth to continue
-				print(f'ExpRanges RUN continuing from {startRanges}')
-		elif os.path.exists(self.tmpdf_fpath): os.remove(self.tmpdf_fpath) #A: Delete tmpdf to start from scratch
-
-		for train_sz in self.ranges['train'][startRanges['train']:]:
-			print(f'ExpRanges run train_sz {train_sz}')
-			frac = 0.4 #TODO: Change frac
+		for (train_sz, k, n) in ( #A: For every combination of ranges
+			ranges
+			for i, ranges in enumerate(itt.product(self.ranges['train'], self.ranges['k'], self.ranges['n']))
+			if i >= lastRange #A: Skip combinations already present in df #TODO: Can this be optimized?
+		):
+			print(f'ExpRanges RUN train_sz {train_sz}, k {k}, n {n}')
+			params, frac = (k, n > 0, n, 1000000), 0.4 #TODO: Change niter and frac
 			split_data(self.name, self.path, frac = frac, subset = ceil(train_sz / frac)) #A: Split the data with a subset the correct length for this frac 
-
-			rows = []
-			for k in self.ranges['k'][startRanges['k']:]:
-				for n in self.ranges['n'][startRanges['n']:]:
-					params = (k, n > 0, n, 1000000) #TODO: Change niter
-					rows += self.run_inst(params)
-			pd.DataFrame(rows).to_csv( #A: Append this data to the tmpdf
-				self.tmpdf_fpath,
+			pd.DataFrame(self.run_inst(params)).to_csv( #A: Append this data to the df
+				self.df_fpath,
 				index = False, mode = 'a', 
-				header = not os.path.exists(self.tmpdf_fpath)
+				header = not os.path.exists(self.df_fpath) #A: Only add header on the first time
 			) 
 		#TODO: Can this be optimized using generators?
-		self.df = pd.read_csv(self.tmpdf_fpath) #A: Read from the full tmpdf
-		self.df.to_csv(self.df_fpath, index = False) #A: Write the actual df
-		os.remove(self.tmpdf_fpath) #A: Delete the tmpdf #TODO: Why?
+		self.df = pd.read_csv(self.df_fpath) #A: Read from the full df #TODO: confusion_matrix not read correctly #TODO: Check if read correct file
+		#TODO: Separate running the experiment from managing the df
 
 	def run_inst(self, params):
 		mySys = MySystem(params, self.path) #A: Run c++ code
 		skSys = SkSystem(params, self.path) #A: Run sklearn code
 		return [mySys.metrics.copy(), skSys.metrics.copy()] #A: Add k and n to each row #TODO: Do I have to copy?
-
-	def read(self): #U: Tries to read previous results
-		try:
-			self.df = pd.read_csv(self.df_fpath)
-		except FileNotFoundError: #A: If the file doesn't exist we need to replace anyways
-			print('ExpRanges ERROR file not found, replacing anyways')
-			self.run()
-		else:
-			print('ExpRanges READ')
-			#TODO: confusion_matrix not read correctly
-			#TODO: Check if read correct file
-			pass
 
 	def plot(self): #TODO: Make more specific
 		print('ExpRanges PLOT')
@@ -107,6 +78,5 @@ if __name__ == '__main__':
 	ExpRanges(
 		name,
 		{'k': [1], 'n': [0, 1], 'train': [1, 2, 3]},
-		# {'k': range(10, 51, 10), 'n': range(0, 6 + 1), 'train': range(1000, 10001, 1000)},
-		# mode = 'continue',
+		# {'k': range(10, 51, 10), 'n': range(0, 6 + 1), 'train': range(1000, 10001, 1000)}
 	)
--- analsys/utils.py
+++ analsys/utils.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 
 import os
+import itertools as itt
 
 def get_data_path():
 	return '../data'
