86ccea34898e291189b198612c8e8fae3b60b916
GalileoCap <galileocapp@gmail.com>
2022-05-16 07:12:05 -0300

    FIX: Metrics, labels start from 0

--- analysis/metrics.py
+++ analysis/metrics.py
@@ -2,7 +2,7 @@ from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, r
 from utils import *
 
 def scores(Y_test, predicted):
-	labels, average = range(1, 10), 'macro' #TODO: Labels as a parameter
+	labels, average = range(0, 10), 'macro' #TODO: Labels as a parameter
 	return {
 		'confusionMatrix': confusion_matrix(Y_test, predicted, labels = labels),
 		'accuracy': accuracy_score(Y_test, predicted), #TODO: Balanced?
